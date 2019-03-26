d5db633152429b7048922ecc9dd6309cbc90b6c2
k <710312372@qq.com>
2019-03-26 06:30:38 +0000

    api/user

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    // 'middleware' => "serializer:array"
+    'middleware' => "serializer:array"
 ], function($api) {
 
     $api->group([
@@ -50,5 +50,22 @@ $api->version('v1', [
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
     });
+
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) {
+        // 游客可以访问的接口
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
+    });
+
+
 
 });
