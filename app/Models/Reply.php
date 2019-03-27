8a5797ebcf7874ee9cdae16b8221c5d9bfd13a63
k <710312372@qq.com>
2019-03-27 07:25:22 +0000

    replies destroy

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -16,11 +16,7 @@ class Reply extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function updateReplyCount()
-    {
-        $this->reply_count = $this->replies->count();
-        $this->save();
-    }
+
 
 
 }
