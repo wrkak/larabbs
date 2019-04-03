e04ee7ab21376b0f3e8dac09d936437faab91def
k <710312372@qq.com>
2019-04-03 09:10:49 +0000

    weapp login

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,7 +45,7 @@ class User extends Authenticatable implements MustVerifyEmailContract,JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name','phone', 'email', 'password','introduction', 'avatar', 'weixin_openid', 'weixin_unionid','registration_id'
+        'name','phone', 'email', 'password','introduction', 'avatar', 'weixin_openid', 'weixin_unionid','registration_id','weixin_session_key','weapp_openid',
     ];
 
     /**
