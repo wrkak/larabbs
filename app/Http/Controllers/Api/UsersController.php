da267126f161c0973d600fb9483feb3ae66f0ba1
k <710312372@qq.com>
2019-04-09 06:43:12 +0000

    users show

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -101,4 +101,9 @@ class UsersController extends Controller
         ])
         ->setStatusCode(201);
     }
+
+    public function show(User $user)
+    {
+        return $this->response->item($user, new UserTransformer());
+    }
 }
